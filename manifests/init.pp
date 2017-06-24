1dd6104efcb08b7cb498a579559423678a7668b3
Paladox <thomasmulhall410@yahoo.com>
2017-06-24 14:56:17 +0000

    Fix syntax error
    Change-Id: Ie4817eb9e5e7b2bab74010a8a118a7299cb7a9e3

--- manifests/init.pp
+++ manifests/init.pp
@@ -108,7 +108,7 @@ class icinga2(
         content => template('mail-host-notification.sh.erb'),
         owner   => 'root',
         group   => 'root',
-        mode    => '0755'
+        mode    => '0755',
         notify  => Base::Service_unit['icinga2'],
     }
 
@@ -117,7 +117,7 @@ class icinga2(
         content => template('mail-service-notification.sh.erb'),
         owner   => 'root',
         group   => 'root',
-        mode    => '0755'
+        mode    => '0755',
         notify  => Base::Service_unit['icinga2'],
     }
 
